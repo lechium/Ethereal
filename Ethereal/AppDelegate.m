b191baded6aa099071342d52c6bcf42630b51c89
Kevin Bradley <kevin.w.bradley@me.com>
2021-09-17 16:47:33 -0700

    one more minor commit

--- Ethereal/AppDelegate.m
+++ Ethereal/AppDelegate.m
@@ -65,8 +65,67 @@
     }
 }
 
-- (void)showPlayerViewWithFile:(NSString *)theFile isLocal:(BOOL)isLocal {
+- (void)createPlayerViewForFile:(NSString *)theFile isLocal:(BOOL)isLocal completion:(void (^)(AVPlayerViewController *controller, BOOL success))block {
+    AVPlayerItem *singleItem = nil;
+    if (isLocal){
+        singleItem = [AVPlayerItem playerItemWithURL:[NSURL fileURLWithPath:theFile]];
+    } else {
+        singleItem = [AVPlayerItem playerItemWithURL:[NSURL URLWithString:theFile]];
+    }
+    __block AVQueuePlayer *player = [AVQueuePlayer playerWithPlayerItem:singleItem];
+    [player play];
     
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        //[self checkPlabackStatus:singleItem withFile:theFile isLocal:isLocal];
+        if (singleItem.error != nil){
+            NSLog(@"[Ethereal] %@", [singleItem error]);
+            player = nil;
+            PlayerViewController *playerController = [PlayerViewController new];
+            if (isLocal){
+                playerController.mediaURL = [NSURL fileURLWithPath:theFile];
+            } else {
+                playerController.mediaURL = [NSURL URLWithString:theFile];
+            }
+            if (block){
+                block(playerController, true);
+            } else {
+                [[self topViewController] presentViewController:playerController animated:true completion:nil];
+            }
+            // [[self topViewController] presentViewController:playerController animated:true completion:nil];
+        } else {
+            NSLog(@"[Ethereal] no error occured!");
+            [player pause];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                AVPlayerViewController *playerView = [[AVPlayerViewController alloc] init];
+                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemDidFinishPlaying:) name:AVPlayerItemDidPlayToEndTimeNotification object:singleItem];
+                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemReceivedError:) name:AVPlayerItemNewErrorLogEntryNotification object:singleItem];
+                playerView.player = player;
+                if (block) {
+                    block(playerView, true);
+                } else {
+                    [[self topViewController] presentViewController:playerView animated:YES completion:nil];
+                    [playerView.player play];
+                }
+                
+            });
+            //[singleItem seekToTime:singleItem.duration completionHandler:nil];
+            //player = nil;
+        }
+    });
+}
+
+- (void)showPlayerViewWithFile:(NSString *)theFile isLocal:(BOOL)isLocal {
+    [self createPlayerViewForFile:theFile isLocal:isLocal completion:^(AVPlayerViewController *controller, BOOL success) {
+        if (controller) {
+            if ([controller isKindOfClass:AVPlayerViewController.class]) {
+                [[self topViewController] presentViewController:controller animated:true completion:nil];
+                [controller.player play];
+            } else {
+                [[self topViewController] presentViewController:controller animated:true completion:nil];
+            }
+        }
+    }];
+    return;
     dispatch_async(dispatch_get_main_queue(), ^{
         AVPlayerItem *singleItem = nil;
         if (isLocal){
